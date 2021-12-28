b4a3a5f47412246ecf1631dd19822d2ddf0343d6
Tux8068 <zombiekiller481516@gmail.com>
2021-12-28 20:24:22 +0000

    starter of webhooks

--- src/main/java/fast/ss/main/main.java
+++ src/main/java/fast/ss/main/main.java
@@ -1,22 +1,44 @@
 package fast.ss.main;
 
 import fast.ss.util.NumberUtils;
+import fast.ss.util.WebhookUtil;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class main {
 
-    public static void main(String [] args) throws AWTException, IOException {
+
+    public static void main(String[] args) throws AWTException, IOException {
 
         try {
+            File cfgfile = new File(".screenshots/cfg.txt");
+            if (!cfgfile.createNewFile()) {
+
+            }
+
+            Scanner cfg = new Scanner(new File(".screenshots/cfg.txt"));
+
+            String hookval = cfg.next();
+            String name = cfg.next();
+            String r = cfg.next();
+            String g = cfg.next();
+            String b = cfg.next();
+
+            System.out.println("WEBHOOK: " + hookval);
+
+            WebhookUtil webhook = new WebhookUtil(hookval);
+            webhook.setContent("Fast Screenshotter");
+            webhook.setAvatarUrl("https://your.awesome/image.png");
+            webhook.setUsername(name);
+
 
             Path path = Paths.get(".screenshots");
             Files.createDirectories(path);
@@ -27,17 +49,15 @@ public class main {
 
             FileWriter logger = new FileWriter(".screenshots/log.txt");
 
-                logger.write(System.lineSeparator());
-                logger.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + System.lineSeparator());
-                logger.write("File: " + file.getName() + System.lineSeparator() + file.getAbsolutePath() + System.lineSeparator());
-                logger.write("Created: " + java.time.LocalTime.now() + System.lineSeparator());
-                logger.write(System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                logger.close();
+            logger.write(System.lineSeparator());
+            logger.write("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + System.lineSeparator());
+            logger.write("File: " + file.getName() + System.lineSeparator() + file.getAbsolutePath() + System.lineSeparator());
+            logger.write("Created: " + java.time.LocalTime.now() + System.lineSeparator());
+            logger.write(System.lineSeparator() + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            logger.close();
 
-
-
-
-
+            webhook.addEmbed(new WebhookUtil.EmbedObject().setTitle("Image: " + file.getName()).setColor(new Color(Integer.parseInt(r), Integer.parseInt(g), Integer.parseInt(b))).setImage(""));
+            webhook.execute();
 
 
         } catch (IOException e) {
